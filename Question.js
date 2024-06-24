7e74c5952e3ff2fe578ad240dab65917c96ee38e
Eman__Iqbal <160255293+EmanIqbal620@users.noreply.github.com>
2024-06-24 03:04:31 -0700

    Add files via upload

---
+++ Question.js
@@ -0,0 +1,8 @@
+"use strict";
+//make arrey
+let names = ["ayesha", "iqra", "anila"];
+let message = "would you like to learn typescript";
+//send message
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
